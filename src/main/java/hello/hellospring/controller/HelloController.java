437cafca16fb22fd039c85ca7d47dff9a2de46d0
Daae-Kim <wlsemrdl1212@naver.com>
2024-03-10 18:30:24 +0900

    정적컨텐츠와 api로 객체 넘겨보기

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,47 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        } }
+} //{name: spring}
+//@RequestParam 으로 http 파라미터를 받을 수 있다.
+//view resolver : templates/{ }.html을 찾는다
+//api 방식 -> json 형식으로 객체를 반환하는것.
